a1280a1ba51696117442135f9dcef266548fb5cf
Yessi Angulo <alexanderangulo917@gmail.com>
2022-12-01 17:28:41 -0500

    up

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BlogController;
+
+Route::post('login', [AuthController::class,'signin']);
+Route::post('register', [AuthController::class, 'signup']);
+     
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('blogs', BlogController::class);
+});
